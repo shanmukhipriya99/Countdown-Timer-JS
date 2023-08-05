cbe96699d4a39c6f56e0109b4d52843af7d72300
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-05 16:43:09 -0700

    calculated sec, min, hour, day

--- js/script.js
+++ js/script.js
@@ -10,5 +10,9 @@ function startCountdown() {
     alert('Please enter a future time!');
     return;
   }
-  console.log(diffTime);
+  let seconds = Math.floor(diffTime / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  let days = Math.floor(hours / 24);
+  console.log(days);
 }
