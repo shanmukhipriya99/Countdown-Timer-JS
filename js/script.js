f38254b86f5a3f5a7c4f7fc3a12776cd7d3a7321
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-05 17:02:58 -0700

    time calculation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 document.getElementById('start-btn').addEventListener('click', () => {
-  startCountdown();
-});
-
-function startCountdown() {
   let inputTime = new Date(document.getElementById('datetime').value).getTime();
   let currentTime = new Date().getTime();
   let diffTime = inputTime - currentTime;
@@ -10,9 +6,20 @@ function startCountdown() {
     alert('Please enter a future time!');
     return;
   }
-  let seconds = Math.floor(diffTime / 1000);
-  let minutes = Math.floor(seconds / 60);
-  let hours = Math.floor(minutes / 60);
-  let days = Math.floor(hours / 24);
-  console.log(days);
+  startCountdown(inputTime, currentTime, diffTime);
+});
+
+function startCountdown(inputTime, currentTime, diffTime) {
+  if (diffTime < 0) {
+    alert('Countdown Completed!');
+    return;
+  }
+  let days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diffTime % (1000 * 60)) / 1000);
+  document.querySelector('.text.days').innerText = days;
+  document.querySelector('.hours').innerText = hours;
+  document.querySelector('.minutes').innerText = minutes;
+  document.querySelector('.seconds').innerText = seconds;
 }
